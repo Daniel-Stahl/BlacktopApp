62da35715566025d23431aac4663b343d4d83854
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2018-06-05 09:10:47 -0700

    upload and download image success

--- Blacktop/Controllers/CafeProfileVC.swift
+++ Blacktop/Controllers/CafeProfileVC.swift
@@ -13,10 +13,16 @@ class CafeProfileVC: UIViewController {
     var ref: DatabaseReference!
     let currentUser = Auth.auth().currentUser?.uid
     
+    let storage = Storage.storage().reference()
+    
     @IBOutlet weak var editCafeProfileButton: UIButton!
     @IBOutlet weak var saveCafeProfileButton: UIButton!
+    
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var changeImageButton: UIButton!
+    var takenImage: UIImage?
+    var imageDownloadURL: String?
+    
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var address: UITextField!
     @IBOutlet weak var city: UITextField!
@@ -25,22 +31,20 @@ class CafeProfileVC: UIViewController {
     @IBOutlet weak var phone: UITextField!
     @IBOutlet weak var website: UITextField!
     
-    @IBOutlet weak var monFrom: TimePicker!
-    @IBOutlet weak var monTo: TimePicker!
-    @IBOutlet weak var tueFrom: TimePicker!
-    @IBOutlet weak var tueTo: TimePicker!
-    @IBOutlet weak var wedFrom: TimePicker!
-    @IBOutlet weak var wedTo: TimePicker!
-    @IBOutlet weak var thuFrom: TimePicker!
-    @IBOutlet weak var thuTo: TimePicker!
-    @IBOutlet weak var friFrom: TimePicker!
-    @IBOutlet weak var friTo: TimePicker!
-    @IBOutlet weak var satFrom: TimePicker!
-    @IBOutlet weak var satTo: TimePicker!
-    @IBOutlet weak var sunFrom: TimePicker!
-    @IBOutlet weak var sunTo: TimePicker!
-    
-    
+    @IBOutlet weak var monOpen: TimePicker!
+    @IBOutlet weak var monClose: TimePicker!
+    @IBOutlet weak var tueOpen: TimePicker!
+    @IBOutlet weak var tueClose: TimePicker!
+    @IBOutlet weak var wedOpen: TimePicker!
+    @IBOutlet weak var wedClose: TimePicker!
+    @IBOutlet weak var thuOpen: TimePicker!
+    @IBOutlet weak var thuClose: TimePicker!
+    @IBOutlet weak var friOpen: TimePicker!
+    @IBOutlet weak var friClose: TimePicker!
+    @IBOutlet weak var satOpen: TimePicker!
+    @IBOutlet weak var satClose: TimePicker!
+    @IBOutlet weak var sunOpen: TimePicker!
+    @IBOutlet weak var sunClose: TimePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,18 +76,21 @@ class CafeProfileVC: UIViewController {
         changeImageButton.isHidden = false
         editCafeProfileButton.isHidden = true
         saveCafeProfileButton.isHidden = false
-        name.isEnabled = true
-        address.isEnabled = true
-        city.isEnabled = true
-        state.isEnabled = true
-        zipcode.isEnabled = true
-        phone.isEnabled = true
-        website.isEnabled = true
+        enableTextField()
     }
     
     func showcafeInfo() {
-        FirebaseService.instance.refUsers.child((Auth.auth().currentUser?.uid)!).observe(.value) { (Datasnapshot) in
-            let data = Datasnapshot.value as! [String: Any]
+        let imageRef = storage.child("photos").child(currentUser!)
+        let downloadTask = imageRef.getData(maxSize: 512 * 512) { (data, error) in
+            if let data = data {
+                let image = UIImage(data: data)
+                self.profileImage.image = image
+            }
+            print(error ?? "No error")
+        }
+        
+        ref.child("users").child(currentUser!).observe(.value) { (Datasnapshot) in
+            guard let data = Datasnapshot.value as? [String: Any] else { return }
             let cafeName = data["name"] as? String
             let cafePhone = data["phone"] as? String
             let cafeWebsite = data["website"] as? String
@@ -94,50 +101,127 @@ class CafeProfileVC: UIViewController {
         }
         
         ref.child("users").child(currentUser!).child("location").observe(.value) { (Datasnapshot) in
-            let data = Datasnapshot.value as! [String: Any]
+            guard let data = Datasnapshot.value as? [String: Any] else { return }
             let cafeAddress = data["address"] as? String
             let cafeCity = data["city"] as? String
             let cafeState = data["state"] as? String
             let cafeZipcode = data["zipcode"] as? String
-            
             
             self.address.text = cafeAddress
             self.city.text = cafeCity
             self.state.text = cafeState
             self.zipcode.text = cafeZipcode
         }
+        
+        ref.child("users").child(currentUser!).child("hours").observe(.value) { (Datasnapshot) in
+            guard let data = Datasnapshot.value as? [String: Any] else { return }
+            let monCafeOpen = data["monOpen"] as? String
+            let monCafeClose = data["monClose"] as? String
+            let tueCafeOpen = data["tueOpen"] as? String
+            let tueCafeClose = data["tueClose"] as? String
+            let wedCafeOpen = data["wedOpen"] as? String
+            let wedCafeClose = data["wedClose"] as? String
+            let thuCafeOpen = data["thuOpen"] as? String
+            let thuCafeClose = data["thuClose"] as? String
+            let friCafeOpen = data["friOpen"] as? String
+            let friCafeClose = data["friClose"] as? String
+            let satCafeOpen = data["satOpen"] as? String
+            let satCafeClose = data["satClose"] as? String
+            let sunCafeOpen = data["sunOpen"] as? String
+            let sunCafeClose = data["sunClose"] as? String
+            
+            self.monOpen.text = monCafeOpen
+            self.monClose.text = monCafeClose
+            self.tueOpen.text = tueCafeOpen
+            self.tueClose.text = tueCafeClose
+            self.wedOpen.text = wedCafeOpen
+            self.wedClose.text = wedCafeClose
+            self.thuOpen.text = thuCafeOpen
+            self.thuClose.text = thuCafeClose
+            self.friOpen.text = friCafeOpen
+            self.friClose.text = friCafeClose
+            self.satOpen.text = satCafeOpen
+            self.satClose.text = satCafeClose
+            self.sunOpen.text = sunCafeOpen
+            self.sunClose.text = sunCafeClose
+        }
 
     }
     
     @IBAction func pressedSaveButton(_ sender: Any) {
-//        guard let cafeName = name.text else { return }
-//        guard let cafeAddress = address.text else { return }
-//        guard let cafeCity = city.text else { return }
-//        guard let cafeState = state.text else { return }
-//        guard let cafeZipcode = zipcode.text else { return }
-//        guard let cafePhone = phone.text else { return }
-//        guard let cafeWebsite = website.text else { return }
-        guard let monFrom = monFrom.text else { return }
-        guard let monTo = monTo.text else { return }
-        guard let tueFrom = tueFrom.text else { return }
-        guard let tueTo = tueTo.text else { return }
-        guard let wedFrom = wedFrom.text else { return }
-        guard let wedTo = wedTo.text else { return }
-        guard let thuFrom = thuFrom.text else { return }
-        guard let thuTo = thuTo.text else { return }
-        guard let friFrom = friFrom.text else { return }
-        guard let friTo = friTo.text else { return }
-        guard let satFrom = satFrom.text else { return }
-        guard let satTo = satTo.text else { return }
-        guard let sunFrom = sunFrom.text else { return }
-        guard let sunTo = sunTo.text else { return }
+        if let imageData = UIImageJPEGRepresentation(takenImage!, 0.0) {
+            let imageRef = storage.child("photos").child(currentUser!)
+            
+            imageRef.putData(imageData).observe(.success) { (Imagesnapshot) in
+                self.imageDownloadURL = Imagesnapshot.metadata?.downloadURL()?.absoluteString
+            }
+        }
         
-        let cafeDetails = ["name": name.text!, "location": ["address": address.text!, "city": city.text!, "state": state.text!, "zipcode": zipcode.text!], "phone": phone.text!, "website": website.text!, "hours": ["monFrom": monFrom, "monTo": monTo, "tueFrom": tueFrom]] as [String : Any]
-        FirebaseService.instance.saveCafeInfo(cafeData: cafeDetails)
-        
+        let cafeDetails = ["name": name.text!, "location": ["address": address.text!, "city": city.text!, "state": state.text!, "zipcode": zipcode.text!], "phone": phone.text!, "website": website.text!, "hours": ["monOpen": monOpen.text!, "monClose": monClose.text!, "tueOpen": tueOpen.text!, "tueClose": tueClose.text!, "wedOpen": wedOpen.text!, "wedClose": wedClose.text!, "thuOpen": thuOpen.text!, "thuClose": thuClose.text!, "friOpen": friOpen.text!, "friClose": friClose.text!, "satOpen": satOpen.text!, "satClose": satClose.text!, "sunOpen": sunOpen.text!, "sunClose": sunClose.text!]] as [String : Any]
+//        FirebaseService.instance.saveCafeInfo(cafeData: cafeDetails)
+        ref.child("users").child(currentUser!).updateChildValues(cafeDetails)
+            
         self.changeImageButton.isHidden = true
         self.editCafeProfileButton.isHidden = false
         self.saveCafeProfileButton.isHidden = true
+        disableTextField()
+    }
+    
+    @IBAction func pressedChangeImageButton(_ sender: Any) {
+        showImageSheet()
+    }
+    
+    func camera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraController = UIImagePickerController()
+            cameraController.delegate = self
+            cameraController.sourceType = .camera
+            self.present(cameraController, animated: true, completion: nil)
+        }
+    }
+    
+    func photoLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let photoLibraryController = UIImagePickerController()
+            photoLibraryController.delegate = self
+            photoLibraryController.sourceType = .photoLibrary
+            self.present(photoLibraryController, animated: true, completion: nil)
+        }
+    }
+    
+    func showImageSheet() {
+        
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.camera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { (alert:UIAlertAction!) -> Void in
+            self.photoLibrary()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+}
+
+extension CafeProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imageBG = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.takenImage = imageBG
+        self.profileImage.image = takenImage
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension CafeProfileVC {
+    func disableTextField() {
         name.isEnabled = false
         address.isEnabled = false
         city.isEnabled = false
@@ -145,18 +229,48 @@ class CafeProfileVC: UIViewController {
         zipcode.isEnabled = false
         phone.isEnabled = false
         website.isEnabled = false
+        monOpen.isEnabled = false
+        monClose.isEnabled = false
+        tueOpen.isEnabled = false
+        tueClose.isEnabled = false
+        wedOpen.isEnabled = false
+        wedClose.isEnabled = false
+        thuOpen.isEnabled = false
+        thuClose.isEnabled = false
+        friOpen.isEnabled = false
+        friClose.isEnabled = false
+        satOpen.isEnabled = false
+        satClose.isEnabled = false
+        sunOpen.isEnabled = false
+        sunClose.isEnabled = false
     }
-
     
-    
-    @IBAction func pressedChangeImageButton(_ sender: Any) {
+    func enableTextField() {
+        name.isEnabled = true
+        address.isEnabled = true
+        city.isEnabled = true
+        state.isEnabled = true
+        zipcode.isEnabled = true
+        phone.isEnabled = true
+        website.isEnabled = true
+        monOpen.isEnabled = true
+        monClose.isEnabled = true
+        tueOpen.isEnabled = true
+        tueClose.isEnabled = true
+        wedOpen.isEnabled = true
+        wedClose.isEnabled = true
+        thuOpen.isEnabled = true
+        thuClose.isEnabled = true
+        friOpen.isEnabled = true
+        friClose.isEnabled = true
+        satOpen.isEnabled = true
+        satClose.isEnabled = true
+        sunOpen.isEnabled = true
+        sunClose.isEnabled = true
     }
     
-    
-    
+    // refactor firebaseservice here
 }
-
-
 
 
 
--- Blacktop/Controllers/ProfileVC.swift
+++ Blacktop/Controllers/ProfileVC.swift
@@ -27,26 +27,10 @@ class ProfileVC: UIViewController {
                 }
             }
     }
-    
-    
-    
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        DataService.instance.REF_USERS.child((Auth.auth().currentUser?.uid)!).observeSingleEvent(of: .value) { (Snapshot) in
-//            let data = Snapshot.value as! [String: Any]
-//            let userRole = data["role"] as! String
-//
-//            if userRole == "cafe" {
-//                self.childVC.isHidden = false
-//            } else {
-//                self.childVC.isHidden = true
-//            }
-//        }
-//    }
 
     @IBAction func pressedExitButton(_ sender: Any) {
         let logoutPopup = UIAlertController(title: "Logout?", message: "Are you sure you want to logout?", preferredStyle: .actionSheet)
+        let logoutCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         let logoutAction = UIAlertAction(title: "Logout?", style: .destructive) { (buttonTapped) in
             
             do {
@@ -57,6 +41,7 @@ class ProfileVC: UIViewController {
                 print(error)
             }
         }
+        logoutPopup.addAction(logoutCancel)
         logoutPopup.addAction(logoutAction)
         present(logoutPopup, animated: true, completion: nil)
     }
